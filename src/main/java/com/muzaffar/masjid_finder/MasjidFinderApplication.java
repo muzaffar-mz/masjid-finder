a0bc2becbc26c2a8c53a969a694a44f8fc28e3d1
muzaffar-mz <mmuratov94@gmail.com>
2024-12-23 20:45:29 +0500

    init environment

---
+++ src/main/java/com/muzaffar/masjid_finder/MasjidFinderApplication.java
@@ -0,0 +1,13 @@
+package com.muzaffar.masjid_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasjidFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasjidFinderApplication.class, args);
+	}
+
+}
