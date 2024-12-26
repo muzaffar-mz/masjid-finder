3ec410a4e869562a00dd3c61d211315b135b5f3b
muzaffar-mz <mmuratov94@gmail.com>
2024-12-26 22:12:21 +0500

    Final configs of tg bot settings

--- src/main/java/com/muzaffar/masjidfinder/bot/Telegram.java
+++ src/main/java/com/muzaffar/masjidfinder/bot/Telegram.java
@@ -2,22 +2,54 @@ package com.muzaffar.masjidfinder.bot;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.longpolling.BotSession;
+import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
+import org.telegram.telegrambots.longpolling.starter.AfterBotRegistration;
+import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
-
-import java.util.List;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class Telegram implements LongPollingSingleThreadUpdateConsumer {
+public class Telegram implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
 
     private final TelegramClient telegramClient;
+    private final UpdateMapper updateMapper;
+    @Value("${telegram.token}")
+    private String token;
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public LongPollingUpdateConsumer getUpdatesConsumer() {
+        return this;
+    }
 
     @Override
     public void consume(Update update) {
-        System.out.println(update.toString());
+        try {
+            handle(update);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
+
+    private void handle(Update update) throws TelegramApiException {
+        final var sendMessages = updateMapper.map(update);
+        telegramClient.execute(sendMessages);
+    }
+
+    @AfterBotRegistration
+    public void afterRegistration(BotSession botSession) {
+        System.out.println("Registered bot running state is: " + botSession.isRunning());
+    }
+
 }
--- src/main/java/com/muzaffar/masjidfinder/config/TelegramConfig.java
+++ src/main/java/com/muzaffar/masjidfinder/config/TelegramConfig.java
@@ -1,13 +1,12 @@
 package com.muzaffar.masjidfinder.config;
 
 
-import com.muzaffar.masjidfinder.bot.Telegram;
+import com.muzaffar.masjidfinder.bot.UpdateMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
-import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 /**
@@ -15,32 +14,15 @@ import org.telegram.telegrambots.meta.generics.TelegramClient;
  */
 
 @Configuration
+@RequiredArgsConstructor
 public class TelegramConfig {
 
     @Value("${telegram.token}")
     private String token;
 
     @Bean
-    public TelegramBotsLongPollingApplication botsLongPollingApplication() {
-        try {
-            TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
-            botsApplication.registerBot(token, new Telegram());
-            return botsApplication;
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Bean
     public TelegramClient telegramClient() {
         return new OkHttpTelegramClient(token);
     }
-
-    @Bean
-    public Telegram telegram() {
-        return new Telegram();
-    }
-
 
 }
